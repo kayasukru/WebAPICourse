c8d1b843ac4e859e4922c40304b5746a0b1b3822
Sukru Kaya <kayasukru@gmail.com>
2024-07-09 20:31:34 +0300

    05.03-Http_Gets_For_Employees

---
+++ Service.Contracts/IEmployeeService.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Contracts
+{
+    public interface IEmployeeService
+    {
+        IEnumerable<Employee> GetAllEmployeesByProjectId(Guid projectId, bool trackChanges);
+        Employee GetOneEmployeeByProjectId(Guid projectId, Guid employeeId, bool trackChanges);
+    }
+}
